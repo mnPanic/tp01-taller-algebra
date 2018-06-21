e942d9a7e885d8ce4ae535fff9b158b5bad946f6
mnPanic <panicmanu@gmail.com>
2018-06-20 23:27:32 -0300

    update test layout

--- alg1-tp.hs
+++ alg1-tp.hs
@@ -28,6 +28,7 @@
 --- | Casos de test
 ---- | Parte A
 ---- | Parte B
+---- | General
 
 -----------------------------------------------------------------------
 --                               Tests
@@ -48,7 +49,7 @@
 --     de la función 1 se pueden testear con la funcion que las engloba
 --     a todas, 'testA_i'.
 -- 
--- Todos los tests pueden correrse con 'testA'
+-- Todos los tests pueden correrse con 'test'
 -- Los de la parte A con 'testA',
 -- y los de la parte B con 'testB'
 
@@ -262,8 +263,114 @@ cualesSonCaminosDeSalida campo (x:xs)
 salidasEnKDesp :: CampoMinado -> Integer -> Conjunto Camino 
 salidasEnKDesp campo k = cualesSonCaminosDeSalida campo (caminosPosiblesDeLongitud k)
 
--------------------------- | Casos de test | --------------------------
---- | Elementos de prueba | ---
+-----------------------------------------------------------------------
+--                              PARTE B
+-----------------------------------------------------------------------
+-------------------------- | Implementación | -------------------------
+
+----------------------- FUNCION 1: recorrido -----------------------
+--- | PRINCIPAL | ---
+--- recorrido: Dado un tablero y una posición p, devuelve una lista que contiene
+--             las posiciones por las que pasará un AF si se lo coloca inicialmente
+--             sobre p. Tener en cuenta que puede tratarse de una lista infinita,
+--             cuya head es p.
+recorrido :: TableroAF -> Posicion -> [Posicion]
+recorrido tablero pos 
+        | not (posValida tablero pos) = []      -- Si el AF esta fuera del tablero, terminó su recorrido.
+        | otherwise = pos : (recorrido tablero siguientePos)
+        where flechaEnPosicion = valor tablero pos  
+              siguientePos = desplazar pos flechaEnPosicion
+
+----------------------- FUNCION 2: escapaDelTablero -----------------------
+--- | DESCRIPCIÓN | ---
+-- Dado un tablero y una posición p, determina si al colocar un AF en p, 
+-- el AF escapará del tablero o entrará en un loop infinito.
+
+--- | AUXILIARES | ---
+-- tieneRepetidosAux: Guarda los elementos de una lista en una auxiliar, para ver si
+--                    en la original hay repetidos
+tieneRepetidosAux :: Eq a => [a] -> [a] -> Bool
+tieneRepetidosAux [] _ = False          -- Una lista vacía nunca tiene repetidos
+tieneRepetidosAux (x:xs) y = (x `contenidoEn` y) || (tieneRepetidosAux xs (x:y))
+
+--- tieneRepetidos: Dada una lista de elementos, dice alguno está repetido.
+--  Ej. tieneRepetidos [1, 2, 3]    ~> False
+--      tieneRepetidos [1, 2, 3, 1] ~> True
+tieneRepetidos :: Eq a => [a] -> Bool
+tieneRepetidos lista = tieneRepetidosAux lista []
+
+--- | PRINCIPAL | ---
+--- escapaDelTablero: Dado un tablero y una posición p, determina si al colocar
+--                    un AF en p, el AF escapará del tablero o entrará en un loop infinito.
+escapaDelTablero :: TableroAF -> Posicion -> Bool
+escapaDelTablero tablero pos = not (tieneRepetidos (recorrido tablero pos))
+
+-- Nota: Con una lista infinita el unico caso en el cual tieneRepetidos no termina,
+--       es en el que en esa lista no hay elementos repetidos.
+--       En escapaDelTablero eso no se da nunca. Pues si un camino no tiene repetidos,
+--       saldría del tablero en una cantidad finita de desplazamientos
+
+----------------------- FUNCION 3: cantidadDePasosParaSalir -----------------------
+--- | DESCRIPCIÓN | ---
+-- Dado un tablero y una posición p, devuelve cuántas veces tiene que desplazarse 
+-- un AF para escapar del tablero si inicialmente lo colocamos en p. Esto incluye 
+-- al último desplazamiento.
+
+--- | AUXILIARES | ---
+--- cambiarValor: Dada una lista, un elemento y una posición,
+--                Reemplaza el elemento en dicha posición por el dado.
+--  Nota: Si el indice es mayor al tamaño de la lista,
+--        el elemento será agregado al final.
+--  Ej. cambiarValor [1,2,3] 6 2 ~> [1,6,3]
+cambiarValor :: [a] -> a -> Integer -> [a]
+cambiarValor [] e _ = [e]
+cambiarValor (x:xs) e 1 = e : xs
+cambiarValor (x:xs) e i = x : (cambiarValor xs e (i - 1))
+
+--- cambiarValorEnMatriz: Dada una matriz, un elemento y una posición,
+--                        Reemplaza el elemento en dicha posición por el dado.
+--  Ej. cambiarValorEnMatriz [[1, 2, 3],[4, 5, 6],[7, 8, 9]] 0 (2,3) ~> [[1,2,3],[4,5,0],[7,8,9]]
+--  Nota: Sabemos que explota para posiciones fuera del tamaño de la matriz, pero no nos interesa
+--        ya que no le damos ese uso. (Nunca será necesario cambiar el valor de una posición con
+--        el AF fuera del tablero)
+cambiarValorEnMatriz :: [[a]] -> a -> Posicion -> [[a]]
+cambiarValorEnMatriz matriz e (i, j) = cambiarValor matriz (cambiarValor fila e j) i
+    where fila = iesimo matriz i
+
+--- rotarSentidoHorario: Dado un desplazamiento, devuelve ese desplazamiento rotado
+--                       en sentido horario.
+rotarSentidoHorario :: Desplazamiento -> Desplazamiento
+rotarSentidoHorario Arriba    = Derecha
+rotarSentidoHorario Derecha   = Abajo
+rotarSentidoHorario Abajo     = Izquierda
+rotarSentidoHorario Izquierda = Arriba
+
+--- actualizarPosicionEnTablero: Dado un tablero y una posicion,
+--                               devuelve un tablero con esa posicion actualizada
+--                               (rotada en sentido horario)
+actualizarPosicionEnTablero :: TableroAF -> Posicion -> TableroAF
+actualizarPosicionEnTablero tablero pos = cambiarValorEnMatriz tablero valorActualizado pos
+    where valorActualizado = rotarSentidoHorario (valor tablero pos)
+
+
+--- | PRINCIPAL | ---
+--- cantidadDePasosParaSalir: Dado un tablero y una posición p, devuelve cuántas
+--                            veces tiene que desplazarse un AF para escapar del
+--                            tablero si inicialmente lo colocamos en p. 
+--                            Esto incluye al último desplazamiento.
+cantidadDePasosParaSalir :: TableroAF -> Posicion -> Integer
+cantidadDePasosParaSalir tablero pos 
+    | not (posValida tablero pos) = 0           -- Si el AF ya esta fuera del tablero, termino el recorrido
+    | otherwise = 1 + cantidadDePasosParaSalir tableroActualizado siguientePos
+    where tableroActualizado = actualizarPosicionEnTablero tablero pos
+          flechaEnPosicion = valor tablero pos
+          siguientePos = desplazar pos flechaEnPosicion
+
+-----------------------------------------------------------------------
+--                               TESTS
+-----------------------------------------------------------------------
+----------------------- | Elementos de prueba | -----------------------
+
 tableroPrueba :: Tablero Integer
 tableroPrueba = [ [0, 0, 0],
                   [0, 0, 0],
@@ -280,7 +387,11 @@ campoPrueba = [ [False, False, True],
 caminoPrueba2 :: Camino
 caminoPrueba2 = [Abajo, Abajo, Derecha, Derecha]
 
+taf3 = [ [Derecha,     Abajo, Abajo],
+         [Arriba,  Izquierda, Abajo],
+         [Abajo,   Izquierda, Izquierda] ]
 
+----------------------------- | PARTE A | -----------------------------
 ------ | FUNCION 1 | ------
 --- desplazar
 testDesplazar :: Bool
@@ -458,7 +569,7 @@ testA_iv =
     testListaDeElementos
 
 
----- Parte A en general
+------ | GENERAL A | ------
 testA :: Bool
 testA =
     testA_i   &&
@@ -466,115 +577,7 @@ testA =
     testA_iii &&
     testA_iv
 
------------------------------------------------------------------------
---                              PARTE B
------------------------------------------------------------------------
--------------------------- | Implementación | -------------------------
-
------------------------ FUNCION 1: recorrido -----------------------
---- | PRINCIPAL | ---
---- recorrido: Dado un tablero y una posición p, devuelve una lista que contiene
---             las posiciones por las que pasará un AF si se lo coloca inicialmente
---             sobre p. Tener en cuenta que puede tratarse de una lista infinita,
---             cuya head es p.
-recorrido :: TableroAF -> Posicion -> [Posicion]
-recorrido tablero pos 
-        | not (posValida tablero pos) = []      -- Si el AF esta fuera del tablero, terminó su recorrido.
-        | otherwise = pos : (recorrido tablero siguientePos)
-        where flechaEnPosicion = valor tablero pos  
-              siguientePos = desplazar pos flechaEnPosicion
-
------------------------ FUNCION 2: escapaDelTablero -----------------------
---- | DESCRIPCIÓN | ---
--- Dado un tablero y una posición p, determina si al colocar un AF en p, 
--- el AF escapará del tablero o entrará en un loop infinito.
-
---- | AUXILIARES | ---
--- tieneRepetidosAux: Guarda los elementos de una lista en una auxiliar, para ver si
---                    en la original hay repetidos
-tieneRepetidosAux :: Eq a => [a] -> [a] -> Bool
-tieneRepetidosAux [] _ = False          -- Una lista vacía nunca tiene repetidos
-tieneRepetidosAux (x:xs) y = (x `contenidoEn` y) || (tieneRepetidosAux xs (x:y))
-
---- tieneRepetidos: Dada una lista de elementos, dice alguno está repetido.
---  Ej. tieneRepetidos [1, 2, 3]    ~> False
---      tieneRepetidos [1, 2, 3, 1] ~> True
-tieneRepetidos :: Eq a => [a] -> Bool
-tieneRepetidos lista = tieneRepetidosAux lista []
-
---- | PRINCIPAL | ---
---- escapaDelTablero: Dado un tablero y una posición p, determina si al colocar
---                    un AF en p, el AF escapará del tablero o entrará en un loop infinito.
-escapaDelTablero :: TableroAF -> Posicion -> Bool
-escapaDelTablero tablero pos = not (tieneRepetidos (recorrido tablero pos))
-
--- Nota: Con una lista infinita el unico caso en el cual tieneRepetidos no termina,
---       es en el que en esa lista no hay elementos repetidos.
---       En escapaDelTablero eso no se da nunca. Pues si un camino no tiene repetidos,
---       saldría del tablero en una cantidad finita de desplazamientos
-
------------------------ FUNCION 3: cantidadDePasosParaSalir -----------------------
---- | DESCRIPCIÓN | ---
--- Dado un tablero y una posición p, devuelve cuántas veces tiene que desplazarse 
--- un AF para escapar del tablero si inicialmente lo colocamos en p. Esto incluye 
--- al último desplazamiento.
-
---- | AUXILIARES | ---
---- cambiarValor: Dada una lista, un elemento y una posición,
---                Reemplaza el elemento en dicha posición por el dado.
---  Nota: Si el indice es mayor al tamaño de la lista,
---        el elemento será agregado al final.
---  Ej. cambiarValor [1,2,3] 6 2 ~> [1,6,3]
-cambiarValor :: [a] -> a -> Integer -> [a]
-cambiarValor [] e _ = [e]
-cambiarValor (x:xs) e 1 = e : xs
-cambiarValor (x:xs) e i = x : (cambiarValor xs e (i - 1))
-
---- cambiarValorEnMatriz: Dada una matriz, un elemento y una posición,
---                        Reemplaza el elemento en dicha posición por el dado.
---  Ej. cambiarValorEnMatriz [[1, 2, 3],[4, 5, 6],[7, 8, 9]] 0 (2,3) ~> [[1,2,3],[4,5,0],[7,8,9]]
---  Nota: Sabemos que explota para posiciones fuera del tamaño de la matriz, pero no nos interesa
---        ya que no le damos ese uso. (Nunca será necesario cambiar el valor de una posición con
---        el AF fuera del tablero)
-cambiarValorEnMatriz :: [[a]] -> a -> Posicion -> [[a]]
-cambiarValorEnMatriz matriz e (i, j) = cambiarValor matriz (cambiarValor fila e j) i
-    where fila = iesimo matriz i
-
---- rotarSentidoHorario: Dado un desplazamiento, devuelve ese desplazamiento rotado
---                       en sentido horario.
-rotarSentidoHorario :: Desplazamiento -> Desplazamiento
-rotarSentidoHorario Arriba    = Derecha
-rotarSentidoHorario Derecha   = Abajo
-rotarSentidoHorario Abajo     = Izquierda
-rotarSentidoHorario Izquierda = Arriba
-
---- actualizarPosicionEnTablero: Dado un tablero y una posicion,
---                               devuelve un tablero con esa posicion actualizada
---                               (rotada en sentido horario)
-actualizarPosicionEnTablero :: TableroAF -> Posicion -> TableroAF
-actualizarPosicionEnTablero tablero pos = cambiarValorEnMatriz tablero valorActualizado pos
-    where valorActualizado = rotarSentidoHorario (valor tablero pos)
-
-
---- | PRINCIPAL | ---
---- cantidadDePasosParaSalir: Dado un tablero y una posición p, devuelve cuántas
---                            veces tiene que desplazarse un AF para escapar del
---                            tablero si inicialmente lo colocamos en p. 
---                            Esto incluye al último desplazamiento.
-cantidadDePasosParaSalir :: TableroAF -> Posicion -> Integer
-cantidadDePasosParaSalir tablero pos 
-    | not (posValida tablero pos) = 0           -- Si el AF ya esta fuera del tablero, termino el recorrido
-    | otherwise = 1 + cantidadDePasosParaSalir tableroActualizado siguientePos
-    where tableroActualizado = actualizarPosicionEnTablero tablero pos
-          flechaEnPosicion = valor tablero pos
-          siguientePos = desplazar pos flechaEnPosicion
-
--------------------------- | Casos de test | --------------------------
---- | Elementos de prueba | ---
-taf3 = [ [Derecha,     Abajo, Abajo],
-         [Arriba,  Izquierda, Abajo],
-         [Abajo,   Izquierda, Izquierda] ]
-
+----------------------------- | PARTE B | -----------------------------
 ------ | FUNCION 1 | ------
 --- recorrido
 testRecorrido :: Bool
@@ -672,9 +675,16 @@ testB_iii =
     testActualizarPosicionEnTablero &&
     testCantidadDePasosParaSalir
 
------- | GENERAL | ------
+------ | GENERAL B | ------
 testB :: Bool
 testB =
     testB_i  &&
     testB_ii &&
     testB_iii
+
+----------------------------- | GENERAL | -----------------------------
+--- TODOS los tests
+test :: Bool
+test = 
+    testA &&
+    testB
